dbe30559678f6393832b1911d627fccaa4d84a62
geenanastanovich <142642960+geenanastanovich@users.noreply.github.com>
2023-12-07 17:09:45 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,47 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+import requests
+from datetime import datetime
 
-@app.route('/')
-def shoppingcart():
-    cart=[]
-    testitem=["Bell Automotive 22-1-53183-A Universal Cheetah Plush Steering Wheel Cover","$23.94","https://www.amazon.com/dp/B0013GSQXS?psc=1&ref=ppx_yo2ov_dt_b_product_details"]
-    testitem2=["Paper Mate Inkjoy Gel Pens, Medium Point, 2-Pack, Black (1951634)","$5.68","https://www.amazon.com/dp/B019QBNZ9Q?psc=1&ref=ppx_yo2ov_dt_b_product_details"]
-    cart.append(testitem)
-    cart.append(testitem2)
-    return render_template("base.html", cart=cart)
+class Retailer:
+    def __init__(self, name, api_key, base_url):
+        self.name = name
+        self.api_key = api_key
+        self.base_url = base_url
+
+    def get_order_data(self):
+        # Replace this with the actual API call to the retailer
+        url = f"{self.base_url}/orders"
+        headers = {"Authorization": f"Bearer {self.api_key}"}
+        response = requests.get(url, headers=headers)
+        return response.json()
+
+class UniversalShoppingCart:
+    def __init__(self):
+        self.orders = []
+
+    def add_order(self, order_data):
+        # Validate and process the order data as needed
+        self.orders.append(order_data)
+
+    def place_orders(self):
+        # Simulate placing orders with a hypothetical external system
+        print("Placing orders with the external system...")
+        print("Orders placed successfully!")
+
+# Replace these with your actual retailer information
+retailer1 = Retailer(name="Retailer1", api_key="your_retailer1_api_key", base_url="https://api.retailer1.com")
+retailer2 = Retailer(name="Retailer2", api_key="your_retailer2_api_key", base_url="https://api.retailer2.com")
+
+# Create a universal shopping cart
+shopping_cart = UniversalShoppingCart()
+
+# Retrieve order data from retailers
+order_data_retailer1 = retailer1.get_order_data()
+order_data_retailer2 = retailer2.get_order_data()
+
+# Add orders to the universal shopping cart
+shopping_cart.add_order(order_data_retailer1)
+shopping_cart.add_order(order_data_retailer2)
+
+# Place orders with the external system
+shopping_cart.place_orders()
+
